83777e6fb837046b80c8ccad80deb5a772f39363
rdc-tech49 <rdc491994@gmail.com>
2025-04-04 08:20:47 +0530

    added password reset settings

--- storemanagement/storemanagement/settings.py
+++ storemanagement/storemanagement/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'techstore.apps.TechstoreConfig',
+
+    #other apps
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rdctech94@gmail.com'
+EMAIL_HOST_PASSWORD = 'xvgf rtkh hqfp gslj'  # Not your Gmail password!
+DEFAULT_FROM_EMAIL = 'Tech Store  <RDC techstore@gmail.com>'
--- storemanagement/techstore/urls.py
+++ storemanagement/techstore/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path('signup/', views.signup, name='signup'),
     path('store_admin/', views.store_admin, name='store_admin'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='techstore/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='techstore/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='techstore/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='techstore/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
